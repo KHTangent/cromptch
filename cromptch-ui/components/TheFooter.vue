06c946e927b51ef9183729efbb960d54922ea26f
KHTangent <contact@khtangent.com>
2024-01-20 12:10:01 +0100

    Update footer

--- cromptch-ui/components/TheFooter.vue
+++ cromptch-ui/components/TheFooter.vue
@@ -1,7 +1,15 @@
 <template>
 	<v-footer>
-		<v-container class="d-flex justify-center">
-			&copy; {{ new Date().getFullYear() }} - KHTangent
+		<v-container class="d-flex align-center justify-space-between">
+			<span class="text-body-1">
+				&copy; {{ new Date().getFullYear() }} - KHTangent, icon by SassyBuns
+			</span>
+			<v-btn
+				variant="text"
+				to="https://github.com/KHTangent/cromptch"
+			>
+				Source Code
+			</v-btn>
 		</v-container>
 	</v-footer>
 </template>
