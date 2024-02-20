6d9d6ddb2518f8f19fcea07e2ed1e595c6eb6902
KHTangent <contact@khtangent.com>
2024-02-20 15:55:42 +0100

    Remove placeholder text

--- cromptch-ui/pages/index.vue
+++ cromptch-ui/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
 	<v-container>
 		<h1 class="text-h1 mb-4">Cromptch: Share recipes</h1>
-		<p class="text-body-1 mb-4">
-			Welcome. This is very WIP. The images are just placeholders from Lorem
-			Picsum.
-		</p>
 		<v-row v-if="recipes.length > 0">
 			<v-col v-for="recipe in recipes" :key="recipe.id" cols="12" md="6" lg="4">
 				<RecipeCard :recipe="recipe" />
