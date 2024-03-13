46ce2015b6cb1c2f2fc4029e9e662d419ecef787
KHTangent <contact@khtangent.com>
2024-03-13 18:13:09 +0100

    List recipes on admin page

--- cromptch-ui/pages/admin.vue
+++ cromptch-ui/pages/admin.vue
@@ -1,17 +1,23 @@
 <template>
 	<v-container>
 		<h1 class="text-h1 mb-4">Cromptch Admin</h1>
+
 		<h2 class="text-h2 my-4">Users</h2>
 		<v-data-table :items="userList"></v-data-table>
+
+		<v-divider class="my-8"></v-divider>
+
+		<h2 class="text-h2 my-4">Recipes</h2>
+		<v-data-table :items="recipeList"></v-data-table>
 	</v-container>
 </template>
 <script setup lang="ts">
 import * as API from "@/scripts/api";
+import * as ApiTypes from "@/scripts/apiTypes";
 import * as AdminAPI from "@/scripts/adminApi";
-import * as APITypes from "@/scripts/apiTypes";
 
 const token = useToken();
-const user = await useLocalUser();
 
 const userList = await AdminAPI.getAllUsers(token.value);
+const recipeList = await API.getRecipeList(ApiTypes.RecipeListSortTypes.NameAscending, 99999);
 </script>
