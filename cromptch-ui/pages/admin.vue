66ee45cafe5a1835ffd2497fd0900e3cba7810c8
KHTangent <contact@khtangent.com>
2024-03-18 18:29:33 +0100

    Add delete button to recipe list on admin page

--- cromptch-ui/pages/admin.vue
+++ cromptch-ui/pages/admin.vue
@@ -8,16 +8,38 @@
 		<v-divider class="my-8"></v-divider>
 
 		<h2 class="text-h2 my-4">Recipes</h2>
-		<v-data-table :items="recipeList"></v-data-table>
+		<v-data-table :items="recipeList">
+			<template v-slot:item.actions="{ item }">
+				<v-btn icon @click="deleteItem(item.id)">
+					<v-icon>
+						{{ icons.mdiTrashCan }}
+					</v-icon>
+				</v-btn>
+			</template>
+		</v-data-table>
 	</v-container>
 </template>
 <script setup lang="ts">
 import * as API from "@/scripts/api";
 import * as ApiTypes from "@/scripts/apiTypes";
 import * as AdminAPI from "@/scripts/adminApi";
+import { mdiTrashCan } from "@mdi/js";
+
+const icons = { mdiTrashCan };
 
 const token = useToken();
 
-const userList = await AdminAPI.getAllUsers(token.value);
-const recipeList = await API.getRecipeList(ApiTypes.RecipeListSortTypes.NameAscending, 99999);
+const userList = ref(await AdminAPI.getAllUsers(token.value));
+var recipeList = ref((await API.getRecipeList(ApiTypes.RecipeListSortTypes.NameAscending, 99999)).map((r: any) => {
+	r["actions"] = "";
+	return r;
+}));
+
+async function deleteItem(id: string) {
+	await AdminAPI.deleteRecipe(token.value, id);
+	recipeList.value = (await API.getRecipeList(ApiTypes.RecipeListSortTypes.NameAscending, 99999)).map((r: any) => {
+		r["actions"] = "";
+		return r;
+	});
+}
 </script>
